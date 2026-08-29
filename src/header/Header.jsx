4796470ecef028d399d44f5b4dcd55e007a8c62a
Caleb <caleb20etinosa@gmail.com>
2026-08-29 08:46:30 +0100

    Responsive Header

--- src/header/Header.jsx
+++ src/header/Header.jsx
@@ -3,7 +3,7 @@ import logo from '../assets/images/favicon.ico'
 function Header() {
 
     return (
-        <header className="fixed top-6 left-1/12 -translate-x-1/2 z-50 rounded-full border border-orange-500 overflow-hidden">
+        <header className="fixed top-6 left-[25%] sm:left-[20%] md:left-[15%] lg:left-[10%] -translate-x-1/2 z-50 rounded-full border border-orange-500 overflow-hidden">
             <nav className="flex items-center gap-5 bg-zinc-800 px-5 py-3 text-white backdrop-blur-md">
                 
                 {/* Logo */}
@@ -27,3 +27,5 @@ function Header() {
 }
 
 export default Header;
+
+// left-1/12 -translate-x-1/2
